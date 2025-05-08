089ecf354ace05e28d9e03d97ddb969b733d8d5b
Admin <vishlin.a.k@gmail.com>
2025-05-08 14:53:01 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        task1(0);
+        task1(1);
+        task1 (3);
     }
+
+    public static void task1(int clientOS) {
+        switch (clientOS) {
+            case 0:
+                System.out.println ("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println ("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println ("Такой операционной системы не существует");
+
+        }
+
+
+    }
+
+
 }
